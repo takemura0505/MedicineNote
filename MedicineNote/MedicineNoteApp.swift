acb1febcd54bc5b49e7bc7997e658b215e5f89c9
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-11 22:03:27 +0900

    初期コミット

--- MedicineNote/MedicineNoteApp.swift
+++ MedicineNote/MedicineNoteApp.swift
@@ -13,8 +13,7 @@ struct MedicineNoteApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HomeView()
         }
     }
 }
