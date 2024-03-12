c404fd260fa05453f9a010100c9f66646d4ee7d7
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-12 17:44:55 +0900

    add buttonView

--- MedicineNote/HomeView.swift
+++ MedicineNote/HomeView.swift
@@ -9,10 +9,76 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            buttonView
+        }
     }
 }
 
 #Preview {
     HomeView()
+}
+
+extension HomeView {
+    
+    var buttonView: some View {
+        ZStack {
+            HStack {
+                drunkButtonView
+                historyButtonView
+            }
+            addButtonView
+                .onTapGesture {
+                    
+                }
+        }
+    }
+    
+    var addButtonView: some View {
+        ZStack {
+            Circle()
+                .frame(height: 100)
+                .padding(.bottom)
+                .foregroundColor(.lightOrange)
+            VStack {
+                Image(systemName: "plus")
+                    .font(.title)
+                    .padding(5)
+                Text("追加")
+                    .font(.caption)
+            }
+        }
+    }
+    
+    var drunkButtonView: some View {
+        ZStack {
+            Rectangle()
+                .frame(height: 80)
+                .foregroundColor(.lightGreen)
+            VStack {
+                Image(systemName: "mug")
+                    .font(.title)
+                    .padding(5)
+                Text("もう飲んだ")
+                    .font(.caption)
+            }
+        }
+    }
+    
+    var historyButtonView: some View {
+        ZStack {
+            Rectangle()
+                .frame(height: 80)
+                .foregroundColor(.lightPink)
+            VStack {
+                Image(systemName: "clock")
+                    .font(.title)
+                    .padding(5)
+                Text("履歴")
+                    .font(.caption)
+            }
+        }
+    }
+    
 }
