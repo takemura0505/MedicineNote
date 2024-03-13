2b2bade8e4ed6d00884d0f644e16d4bd89b0ba7e
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-13 14:45:08 +0900

    fixed code

--- MedicineNote/View/SettingView.swift
+++ MedicineNote/View/SettingView.swift
@@ -13,47 +13,67 @@ struct SettingView: View {
     
     var body: some View {
         VStack(spacing: 50) {
-            HStack {
-                Text("お薬の名前")
-                Spacer()
-                TextField("名前を入力", text: $nameTextField)
-                    .padding()
-                    .frame(width: 200, height: 45)
-                    .background(.lightGray)
-                    .clipShape(Capsule())
-            }
-            .padding()
-            HStack {
-                Text("お薬の量")
-                Spacer()
-                TextField("量を入力", text: $nameTextField)
-                    .padding()
-                    .frame(width: 200, height: 45)
-                    .background(.lightGray)
-                    .clipShape(Capsule())
-            }
-            .padding()
-            HStack {
-                Text("お薬を飲む時間")
-                Spacer()
-                TextField("お薬を飲む時間を入力", text: $nameTextField)
-                    .padding()
-                    .frame(width: 200, height: 45)
-                    .background(.lightGray)
-                    .clipShape(Capsule())
-            }
-            .padding()
-            ZStack {
-                Capsule()
-                    .frame(width: 250, height: 50)
-                Image(systemName: "checkmark")
-                    .foregroundColor(.white)
-            }
-                
+            nameView
+            dosage
+            timeView
+            addButtonView
         }
     }
 }
 
 #Preview {
     SettingView()
+}
+
+extension SettingView {
+    
+    var nameView: some View {
+        HStack {
+            Text("お薬の名前")
+            Spacer()
+            TextField("名前を入力", text: $nameTextField)
+                .padding()
+                .frame(width: 200, height: 45)
+                .background(.lightGray)
+                .clipShape(Capsule())
+        }
+        .padding()
+    }
+    
+    var dosage: some View {
+        HStack {
+            Text("お薬の量")
+            Spacer()
+            TextField("量を入力", text: $nameTextField)
+                .padding()
+                .frame(width: 200, height: 45)
+                .background(.lightGray)
+                .clipShape(Capsule())
+        }
+        .padding()
+    }
+    
+    var timeView: some View {
+        HStack {
+            Text("お薬を飲む時間")
+            Spacer()
+            TextField("お薬を飲む時間を入力", text: $nameTextField)
+                .padding()
+                .frame(width: 200, height: 45)
+                .background(.lightGray)
+                .clipShape(Capsule())
+        }
+        .padding()
+    }
+    
+    var addButtonView: some View {
+        ZStack {
+            Capsule()
+                .frame(width: 250, height: 50)
+                .foregroundColor(Color(uiColor: .label))
+            Text("追加")
+                .foregroundColor(Color(uiColor: .systemBackground))
+        }
+    }
+    
 }
