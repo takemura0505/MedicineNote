8724a3fa18b7d5b25c5ec4b225dd84334187dac2
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-15 15:21:18 +0900

    通知の許可確認を追加

--- MedicineNote/MedicineNoteApp.swift
+++ MedicineNote/MedicineNoteApp.swift
@@ -9,11 +9,27 @@ import SwiftUI
 
 @main
 struct MedicineNoteApp: App {
+    
+    init() {
+        requestNotificationPermission()
+    }
+    
     let persistenceController = PersistenceController.shared
-
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
     }
+    
+    func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("success")
+            } else {
+                print(error)
+            }
+        }
+    }
+    
 }
