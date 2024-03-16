24c5d2813e722e988a438865bc9a67e69656c4cf
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-16 14:49:55 +0900

    アプリがフォアグラウンドでも通知を受け取る処理追加

---
+++ MedicineNote/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MedicineNote
+//
+//  Created by 竹村はるうみ on 2024/03/16.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound, .badge])
+    }
+    
+}
--- MedicineNote/MedicineNoteApp.swift
+++ MedicineNote/MedicineNoteApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct MedicineNoteApp: App {
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     init() {
         requestNotificationPermission()
     }
